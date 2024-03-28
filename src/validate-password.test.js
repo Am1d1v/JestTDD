0eccd55eba6af8fb440c9a720af743dbe3574fd9
Am1d1v <dimadiman1adima@gmail.com>
2024-03-28 16:02:39 +0300

    Jest TDD

---
+++ src/validate-password.test.js
@@ -0,0 +1,23 @@
+import { valudatePassword } from "./validate-password";
+
+describe('Password Palidation', () => {
+
+    // Valid Password
+    it.todo('Should return true for a valid password');
+
+
+    it.todo('Should validate a password contains minimum 8 characters')
+
+
+    it.todo('Should validate a password contains mixed cases')
+
+
+    it.todo('Should validate a password contains digits and characters')
+
+
+    it.todo('Should validate a password contains special characters')
+
+    // Banned Dummy Passwords
+    it.todo('Should validate a password doesnt contant dummy passwords')
+
+});
